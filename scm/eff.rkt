ec5453b50e2c5767952b6bd92ff8dd4e25ae3fd8
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2017-01-25 21:05:53 +0200

    Don't use parameters in with-effect-handler.

--- scm/eff.rkt
+++ scm/eff.rkt
@@ -5,21 +5,20 @@
 
 (define-syntax with-effect-handler
   (syntax-rules ()
-    ((_ (name (effect-class initargs ...)) body ...)
-     (let ((tag (make-continuation-prompt-tag 'eff-handler)))
-       (parameterize ((name (new effect-class
-                                 [prompt-tag tag] initargs ...)))
-         (send (name) run
-               (reset-at tag (send (name) pure (begin body ...)))))))))
+    ((_ (handler (effect-class initargs ...)) body ...)
+     (let* ((tag (make-continuation-prompt-tag 'eff-handler))
+            (handler (new effect-class [prompt-tag tag] initargs ...)))
+       (send handler run
+             (reset-at tag
+               (send handler pure (begin body ...))))))))
 
 (define-syntax send!
   (syntax-rules ()
     ((_ eff-obj method args ...)
-     (shift-at (send (eff-obj) get-prompt-tag) k
-       (let ((v (send (eff-obj) method k args ...)))
-         (abort-current-continuation
-          (send (eff-obj) get-prompt-tag)
-          (lambda () v)))))))
+     (let ((tag (send eff-obj get-prompt-tag)))
+       (shift-at tag k
+         (abort-current-continuation tag
+           (lambda () (send eff-obj method k args ...))))))))
 
 (define eff-handler%
   (class object%
@@ -64,7 +63,6 @@
     (define/override (pure value)
       (cons 'some value))))
 
-
 (define state-handler (interface () lookup update!))
 
 (define mstate%
@@ -95,10 +93,6 @@
 ;;;; Tests ------------------------------------------------------------------...
 
 (test-case "choice"
-  (define c (make-parameter #f))
-  (define c1 (make-parameter (c)))
-  (define c2 (make-parameter (c)))
-
   (check-equal?
     (with-effect-handler (c (always%))
       (let ((x (if (send! c decide) 10 20))
@@ -122,21 +116,18 @@
              (y (if (send! c2 decide) 0 5)))
          (- x y))))
    '((10 5) (20 15))
-   "all-all"))
+   "all-all")
 
-;  (check-equal?
-;   (with-effect-handler (c2 (choose-all%))
-;     (with-effect-handler (c1 (choose-all%))
-;       (let ((x (if (send! c2 decide) 10 20))
-;             (y (if (send! c1 decide) 0 5)))
-;         (- x y))))
-;   '((10 20) (5 15))
-;   "all-lla"))
-    
+  (check-equal?
+   (with-effect-handler (c2 (choose-all%))
+     (with-effect-handler (c1 (choose-all%))
+       (let ((y (if (send! c2 decide) 0 5))
+             (x (if (send! c1 decide) 10 20)))
+         (- x y))))
+   '((10 20) (5 15))
+   "all-lla"))
 
 (test-case "exception"
-  (define e (make-parameter #f))
-  
   (check-equal?
    (with-effect-handler (e (optionalize%))
      (+ 3 (send! e raise 'hell)))
@@ -144,8 +135,6 @@
    "exn:optionalize"))
 
 (test-case "state"
-  (define r (make-parameter #f))
-
   (check-equal?
     (with-effect-handler (r (mstate% [state 3]))
       (send! r update! (add1 (send! r lookup)))
