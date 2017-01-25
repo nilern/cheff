5b14cd3f7fba72ee1984db4d37d7110abff747d7
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2017-01-25 20:31:25 +0200

    Refactor into class definitions + tests.

--- scm/eff.rkt
+++ scm/eff.rkt
@@ -36,18 +36,68 @@
     (define/public (run value)
       value)))
 
-;;;; Examples ------------------------------------------------------------------
+;;;; Effect types (classes) ----------------------------------------------------
 
-(define (choice)
-  (define choice-handler (interface () decide))
-  
+(define choice-handler (interface () decide))
+
+(define always%
+  (class* eff-handler% (choice-handler)
+    (super-new)
+    (define/public (decide k)
+      (k #t))))
+
+(define choose-all%
+  (class* eff-handler% (choice-handler)
+    (super-new)
+    (define/public (decide k)
+      (append (k #t) (k #f)))
+    (define/override (pure value)
+      (list value))))
+
+(define exn-handler (interface () raise))
+
+(define optionalize%
+  (class* eff-handler% (exn-handler)
+    (super-new)
+    (define/public (raise k exn)
+      'none)
+    (define/override (pure value)
+      (cons 'some value))))
+
+
+(define state-handler (interface () lookup update!))
+
+(define mstate%
+  (class* eff-handler% (state-handler)
+    (super-new)
+    (init state)
+    (define -state state)
+    (define/public (lookup k)
+      (lambda (s) ((k s) s)))
+    (define/public (update! k s*)
+      (lambda (s) ((k s*) s*)))
+    (define/override (pure v)
+      (lambda (s) v))
+    (define/override (run f)
+      (f -state))))
+
+(define istate%
+  (class* eff-handler% (state-handler)
+    (super-new)
+    (init state)
+    (define -state state)
+    ;; actually might as well ditch the continuations and use normal `send`:
+    (define/public (lookup k)
+      (k -state))
+    (define/public (update! k s*)
+      (k (set! -state s*)))))
+
+;;;; Tests ------------------------------------------------------------------...
+
+(test-case "choice"
   (define c (make-parameter #f))
-
-  (define always%
-    (class* eff-handler% (choice-handler)
-      (super-new)
-      (define/public (decide k)
-        (k #t))))
+  (define c1 (make-parameter (c)))
+  (define c2 (make-parameter (c)))
 
   (check-equal?
     (with-effect-handler (c (always%))
@@ -55,15 +105,7 @@
             (y (if (send! c decide) 0 5)))
         (- x y)))
     10
-    "choice:always")
-
-  (define choose-all%
-    (class* eff-handler% (choice-handler)
-      (super-new)
-      (define/public (decide k)
-        (append (k #t) (k #f)))
-      (define/override (pure value)
-        (list value))))
+    "always")
 
   (check-equal?
     (with-effect-handler (c (choose-all%))
@@ -71,10 +113,7 @@
             (y (if (send! c decide) 0 5)))
         (- x y)))
     '(10 5 20 15)
-    "choice:all")
-
-  (define c1 (make-parameter (c)))
-  (define c2 (make-parameter (c)))
+    "all")
 
   (check-equal?
    (with-effect-handler (c1 (choose-all%))
@@ -83,7 +122,7 @@
              (y (if (send! c2 decide) 0 5)))
          (- x y))))
    '((10 5) (20 15))
-   "choice:all-all"))
+   "all-all"))
 
 ;  (check-equal?
 ;   (with-effect-handler (c2 (choose-all%))
@@ -92,20 +131,10 @@
 ;             (y (if (send! c1 decide) 0 5)))
 ;         (- x y))))
 ;   '((10 20) (5 15))
-;   "choice:all-lla"))
+;   "all-lla"))
     
 
-(define (exn)
-  (define exn-handler (interface () raise))
-
-  (define optionalize%
-    (class* eff-handler% (exn-handler)
-      (super-new)
-      (define/public (raise k exn)
-        'none)
-      (define/override (pure value)
-        (cons 'some value))))
-  
+(test-case "exception"
   (define e (make-parameter #f))
   
   (check-equal?
@@ -114,24 +143,8 @@
    'none
    "exn:optionalize"))
 
-(define (state)
-  (define state-handler (interface () lookup update!))
-
+(test-case "state"
   (define r (make-parameter #f))
-
-  (define mstate%
-    (class* eff-handler% (state-handler)
-      (super-new)
-      (init state)
-      (define -state state)
-      (define/public (lookup k)
-        (lambda (s) ((k s) s)))
-      (define/public (update! k s*)
-        (lambda (s) ((k s*) s*)))
-      (define/override (pure v)
-        (lambda (s) v))
-      (define/override (run f)
-        (f -state))))
 
   (check-equal?
     (with-effect-handler (r (mstate% [state 3]))
@@ -140,29 +153,9 @@
     4
     "state:monadic")
 
-  (define istate%
-    (class* eff-handler% (state-handler)
-      (super-new)
-      (init state)
-      (define -state state)
-      ;; actually might as well ditch the continuations and use normal `send`:
-      (define/public (lookup k)
-        (k -state))
-      (define/public (update! k s*)
-        (k (set! -state s*)))))
-
   (check-equal?
     (with-effect-handler (r (istate% [state 3]))
       (send! r update! (add1 (send! r lookup)))
       (send! r lookup))
     4
     "state:field"))
-
-;;;; Main ----------------------------------------------------------------------
-
-(define (main)
-  (choice)
-  (exn)
-  (state))
-
-(main)
